7a011d79a5d1ad1a5a16615879509b8691c6bd9f
Harsh Jha <154000633+harshjha047@users.noreply.github.com>
2024-04-11 20:00:14 +0530

    File3.js

--- Animated/File.js
+++ Animated/File.js
@@ -80,3 +80,18 @@ video.addEventListener("click",function(){
 })
 }
 page3Animation();
+function page4Animation(){
+    var sections = document.querySelectorAll(".sec-right")
+
+sections.forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        elem.childNodes[3].style.opacity = 1
+        elem.childNodes[3].play()
+    })
+    elem.addEventListener("mouseleave",function(){
+        elem.childNodes[3].style.opacity = 0
+        elem.childNodes[3].load()
+    })
+})
+}
+page4Animation();
