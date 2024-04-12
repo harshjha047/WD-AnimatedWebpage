545dbf6d58bcec8622549ab69a8fdf93fe3bb5e7
Harsh Jha <154000633+harshjha047@users.noreply.github.com>
2024-04-12 21:06:07 +0530

    Update File.js

--- Animated/File.js
+++ Animated/File.js
@@ -1,134 +1,41 @@
-function locomotiveAnimation(){
+function locomotiveAnimation() {
     gsap.registerPlugin(ScrollTrigger);
 
-// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector("#main"),
+        smooth: true,
 
-const locoScroll = new LocomotiveScroll({
-  el: document.querySelector("#main"),
-  smooth: true
-});
-// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-locoScroll.on("scroll", ScrollTrigger.update);
+        // for tablet smooth
+        tablet: { smooth: true },
 
-// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
-ScrollTrigger.scrollerProxy("#main", {
-  scrollTop(value) {
-    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-  getBoundingClientRect() {
-    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
-  },
-  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
-});
+        // for mobile
+        smartphone: { smooth: true }
+    });
+    locoScroll.on("scroll", ScrollTrigger.update);
 
-
-
-
-
-
-// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
-ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-ScrollTrigger.refresh();
-
-
-};locomotiveAnimation();
-function navAnimation(){
-    document.querySelector("nav").addEventListener("mouseenter",function(){
-    var tl = gsap.timeline()
-    gsap.to("#nav-bottom",{
-        height:"20vh"
-    })
-    tl.to(".nav-part2 h5",{
-        display:"block"
-    })
-    tl.to(".nav-part2 h5 span",{
-        y:0,
-        stagger:{
-            amount:0.6
+    ScrollTrigger.scrollerProxy("#main", {
+        scrollTop(value) {
+            return arguments.length
+                ? locoScroll.scrollTo(value, 0, 0)
+                : locoScroll.scroll.instance.scroll.y;
+        },
+        getBoundingClientRect() {
+            return {
+                top: 0,
+                left: 0,
+                width: window.innerWidth,
+                height: window.innerHeight
+            };
         }
-    })
-})
-document.querySelector("nav").addEventListener("mouseleave",function(){
-    var tl = gsap.timeline()
-    tl.to(".nav-part2 h5 span",{
-        y:25,
-        stagger:{
-            amount:0.2
-        }
-    })
-        tl.to(".nav-part2 h5",{
-        display:"noun",
-        duration:0.1
-    })
-     tl.to("#nav-bottom",{
-        height:0,
-        duration:0.2
-    })})
-};navAnimation();
-function page2Animation(){
-    let rightelem = document.querySelectorAll(".right-elem");
+    });
 
-rightelem.forEach(function(elem){
-    elem.addEventListener("mouseenter",function(){
-       gsap.to(elem.childNodes[3],{
-        opacity:1,
-        scale:1
-       }) 
-    })
-    elem.addEventListener("mouseleave",function(){
-        gsap.to(elem.childNodes[3],{
-            opacity:0,
-            scale:0
-           }) 
-        })
-    elem.addEventListener("mousemove",function(dets){
-        gsap.to(elem.childNodes[3],{
-            x:dets.x-elem.getBoundingClientRect().x-40,
-            y:dets.y-elem.getBoundingClientRect().y-130
-        })
-    })
-})
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
-};page2Animation();
-function page3Animation(){
-var p3center= document.querySelector(".page3-center")
-var video =document.querySelector("#page3 video")
+    ScrollTrigger.refresh();
+}
 
-p3center.addEventListener("click",function(){
-    video.play();
-    gsap.to(video,{
-        transform:"scaleX(1) scaleY(1)",
-        opacity:1,
-        borderRadius:0
-    })
-})
-video.addEventListener("click",function(){
-    video.load();
-    gsap.to(video,{
-        transform:"scaleX(0.7) scaleY(0)",
-        opacity:0,
-        borderRadius:"30px"
-    })
-})
-};page3Animation();
-function page4Animation(){
-    var sections = document.querySelectorAll(".sec-right")
+function loadingAnimation() {
 
-sections.forEach(function(elem){
-    elem.addEventListener("mouseenter",function(){
-        elem.childNodes[3].style.opacity = 1
-        elem.childNodes[3].play()
-    })
-    elem.addEventListener("mouseleave",function(){
-        elem.childNodes[3].style.opacity = 0
-        elem.childNodes[3].load()
-    })
-})
-};page4Animation();
-function loadingAnnimation(){
     var tl = gsap.timeline()
     tl.from("#page1", {
         opacity: 0,
@@ -150,6 +57,125 @@ function loadingAnnimation(){
         duration: 0.5,
         stagger: 0.2
     })
-};loadingAnnimation();
+}
+
+function navAnimation() {
+    var nav = document.querySelector("nav")
+
+    nav.addEventListener("mouseenter", function () {
+        let tl = gsap.timeline()
+
+        tl.to("#nav-bottom", {
+            height: "20vh",
+            duration: 0.5
+        })
+        tl.to(".nav-part2 h5", {
+            display: "block",
+            duration: 0.1
+
+        })
+        tl.to(".nav-part2 h5 span", {
+            y: 0,
+            // duration:0.3,
+            stagger: {
+                amount: 0.5
+            }
+        })
+    })
+    nav.addEventListener("mouseleave", function () {
+        let tl = gsap.timeline()
+        tl.to(".nav-part2 h5 span", {
+            y: 25,
+            stagger: {
+                amount: 0.2
+            }
+        })
+        tl.to(".nav-part2 h5", {
+            display: "none",
+            duration: 0.1
+        })
+        tl.to("#nav-bottom", {
+            height: 0,
+            duration: 0.2
+        })
+    })
+}
+
+function page2Animation() {
+    var rightElems = document.querySelectorAll(".right-elem")
+
+    rightElems.forEach(function (elem) {
+        elem.addEventListener("mouseenter", function () {
 
 
+
+
+            gsap.to(elem.childNodes[3], {
+                opacity: 1,
+                scale: 1
+            })
+        })
+        elem.addEventListener("mouseleave", function () {
+            gsap.to(elem.childNodes[3], {
+                opacity: 0,
+                scale: 0
+            })
+        })
+        elem.addEventListener("mousemove", function (dets) {
+
+            gsap.to(elem.childNodes[3], {
+                x: dets.x - elem.getBoundingClientRect().x - 45,
+                y: dets.y - elem.getBoundingClientRect().y - 100
+            })
+        })
+    })
+}
+
+function page3VideoAnimation() {
+    var page3Center = document.querySelector(".page3-center")
+    var video = document.querySelector("#page3 video")
+
+    page3Center.addEventListener("click", function () {
+        video.play()
+        gsap.to(video, {
+            transform: "scaleX(1) scaleY(1)",
+            opacity: 1,
+            borderRadius: 0
+        })
+    })
+    video.addEventListener("click", function () {
+        video.pause()
+        gsap.to(video, {
+            transform: "scaleX(0.7) scaleY(0)",
+            opacity: 0,
+            borderRadius: "30px"
+        })
+    })
+
+
+    var sections = document.querySelectorAll(".sec-right")
+
+    sections.forEach(function (elem) {
+        elem.addEventListener("mouseenter", function () {
+            elem.childNodes[3].style.opacity = 1
+            elem.childNodes[3].play()
+        })
+        elem.addEventListener("mouseleave", function () {
+            elem.childNodes[3].style.opacity = 0
+            elem.childNodes[3].load()
+        })
+    })
+
+}
+
+
+locomotiveAnimation()
+
+navAnimation()
+
+page2Animation()
+
+page3VideoAnimation()
+
+
+loadingAnimation()
